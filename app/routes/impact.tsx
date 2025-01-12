2d76363bfecc68b9a8963856a82774d31a0ebf5d
eugenedev <eugenesew4@gmail.com>
2025-01-12 20:19:31 +0000

    nimate

--- app/routes/impact.tsx
+++ app/routes/impact.tsx
@@ -2,6 +2,7 @@ import type { MetaFunction } from "@remix-run/node";
 import { Button } from "~/components/ui/button";
 import Footer from "~/components/ui/custom/Footer";
 import Header from "~/components/ui/custom/Header";
+import impact from "~/assets/impact.png";
 
 export const meta: MetaFunction = () => {
   return [
@@ -14,7 +15,9 @@ export default function Impact() {
   return (
     <div className="bg-white h-fit items-center w-screen grid">
       <Header />
-      <div className="z-0"></div>
+      <div className="z-0 bg-oha__green w-full flex justify-center items-center py-32">
+        <img src={impact} alt="impact img" className=" mt-20" />
+      </div>
       <Footer />
     </div>
   );
